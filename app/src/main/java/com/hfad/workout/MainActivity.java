ecc0332d165762972ebae0f22f69d3109a2988a2
Anurag Vishwa <you@example.com>
2017-03-03 21:34:30 +0530

    Initial commit

---
+++ app/src/main/java/com/hfad/workout/MainActivity.java
@@ -0,0 +1,38 @@
+package com.hfad.workout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+import android.support.v4.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.WorkoutListListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id)
+    {
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if(fragmentContainer !=null) {
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+
+        }
+        else{
+            Intent intent= new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID,(int)id);
+        }
+
+    }
+}
